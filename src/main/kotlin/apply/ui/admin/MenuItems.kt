4f9c4447dd442c487118ebadcc132e67b5dc1f84
BeomsooKim <kbsat@naver.com>
2022-09-18 01:38:03 +0900

    콤보박스 개선, 스크롤 제거, 사이드바 유지한 채 라우팅하도록 수정, but 더블클릭 시 중복 된 테이블 나오는 버그, tabs가 달라 여러개가 선택된 것처럼 보이는 버그 수정필요

--- src/main/kotlin/apply/ui/admin/MenuItems.kt
+++ src/main/kotlin/apply/ui/admin/MenuItems.kt
@@ -1,11 +1,10 @@
 package apply.ui.admin
 
 import apply.application.RecruitmentResponse
+import apply.ui.admin.mission.MissionsView
+import apply.ui.admin.selections.SelectionView
 import com.vaadin.flow.component.Component
-import com.vaadin.flow.component.accordion.Accordion
 import com.vaadin.flow.component.combobox.ComboBox
-import com.vaadin.flow.component.details.DetailsVariant
-import com.vaadin.flow.component.html.Anchor
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.component.tabs.Tab
 import com.vaadin.flow.router.RouterLink
@@ -15,15 +14,11 @@ infix fun String.of(navigationTarget: Class<out Component>): MenuItem {
     return SingleMenuItem(this, navigationTarget)
 }
 
-fun String.accordionOf(path: String, recruitments: List<RecruitmentResponse>): MenuItem {
-    return AccordionMenuItem(this, path, recruitments.map { it.toContent() })
-}
-
 fun String.comboBoxOf(recruitments: List<RecruitmentResponse>): MenuItem {
     return ComboBoxMenuItem(this, recruitments.map { it.toContent() })
 }
 
-private fun RecruitmentResponse.toContent(): AccordionContent = AccordionContent(id, title)
+private fun RecruitmentResponse.toContent(): ComboBoxContent = ComboBoxContent(id, title)
 
 sealed class MenuItem(protected val title: String) {
     abstract fun toComponent(): Component
@@ -34,71 +29,48 @@ class SingleMenuItem(
     private val navigationTarget: Class<out Component>
 ) : MenuItem(title) {
     override fun toComponent(): Component {
-        return Tab(RouterLink(title, navigationTarget))
+        return Tab(RouterLink(title, navigationTarget).apply {
+            style.set("justify-content", "center")
+        })
     }
 }
 
-data class AccordionContent(val id: Any, val title: String)
-
-class AccordionMenuItem(
-    title: String,
-    private val path: String,
-    private val contents: List<AccordionContent>
-) : MenuItem(title) {
-    override fun toComponent(): Component {
-        return Accordion().apply {
-            add(title, createTabs()).addThemeVariants(DetailsVariant.REVERSE)
-            close()
-        }
-    }
-
-    private fun createTabs(): Component? {
-        if (contents.isEmpty()) {
-            return null
-        }
-        return createTabs(contents.toTabs(path))
-    }
-
-    private fun List<AccordionContent>.toTabs(path: String): List<Component> {
-        return map { Tab(Anchor("$path/${it.id}", it.title)) }
-    }
-}
+data class ComboBoxContent(val id: Long, val title: String)
 
 class ComboBoxMenuItem(
     placeholder: String,
-    private val contents: List<AccordionContent>
+    private val contents: List<ComboBoxContent>
 ) : MenuItem(placeholder) {
     override fun toComponent(): Component {
         val associateBy = contents.associateBy { it.title }
-        // local storage
-        val layout = VerticalLayout()
 
-        val anchor = Anchor("", "과제 관리")
-        val tab = Tab().apply {
-            add(anchor)
-        }
-        val anchor2 = Anchor("", "선발 과정")
-        val tab2 = Tab().apply {
-            add(anchor2)
+        val layout = VerticalLayout().apply {
+            element.style.set("width", "85%")
         }
 
-        val tabs = createTabs(listOf(tab, tab2))
-        tabs.isVisible = false
+        val missionsLink = RouterLink("과제 관리", MissionsView::class.java, 0)
+        val selectionLink = RouterLink("선발 과정", SelectionView::class.java, 0)
+
+        val hiddenTabs = createTabs(listOf(Tab(missionsLink), Tab(selectionLink)))
+            .apply {
+                element.style.set("margin", "0 auto")
+                isVisible = false
+            }
 
         val comboBox = ComboBox<String>().apply {
             placeholder = title
             setItems(contents.map { it.title })
 
             addValueChangeListener {
-                tabs.isVisible = true
-                val id = associateBy[it.value]?.id
-                anchor.href = "admin/missions/${id}"
-                anchor2.href = "admin/selections/${id}"
+                val id = associateBy[it.value]!!.id
+                hiddenTabs.isVisible = true
+                missionsLink.setRoute(MissionsView::class.java, id)
+                selectionLink.setRoute(SelectionView::class.java, id)
             }
         }
         layout.apply {
             add(comboBox)
-            add(tabs)
+            add(hiddenTabs)
         }
         return layout
     }
